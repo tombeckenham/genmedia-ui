a00e7b90a9b9d47fde240aa176276042630a2835
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2026-07-18 08:56:42 +1000

    Updated shadcn

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,17 +1,14 @@
-'use client'
+import * as React from "react"
 
-import * as React from 'react'
-import { Label as LabelPrimitive } from 'radix-ui'
+import { cn } from "#/lib/utils.ts"
 
-import { cn } from '#/lib/utils.ts'
-
-function Label({ className, ...props }: React.ComponentProps<typeof LabelPrimitive.Root>) {
+function Label({ className, ...props }: React.ComponentProps<"label">) {
   return (
-    <LabelPrimitive.Root
+    <label
       data-slot="label"
       className={cn(
-        'flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
-        className,
+        "flex items-center gap-2 text-xs/relaxed leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
+        className
       )}
       {...props}
     />
